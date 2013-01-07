e5d4226298ea8d070b860c844b69fea511006fce
mr. keith <keith@weedmaps.com>
2013-01-07 15:28:44 -0800

    using centered header layout & added scroll-to-top

--- app/layout.ls
+++ app/layout.ls
@@ -1,10 +1,25 @@
 
 # XXX layout-specific client-side
 
-win = $ window
+threshold = 10 # snap
+w = $ window
 
-win.on 'scroll' ->
-  if win.scrollTop! > 8
+# indicate to stylus that view scrolled
+has-scrolled = ->
+  if w.scrollTop! > threshold
     $ 'body' .add-class 'has-scrolled'
   else
     $ 'body' .remove-class 'has-scrolled'
+
+w.on 'scroll' -> has-scrolled!
+has-scrolled!
+
+
+# attach scroll-to-top
+$ '.scroll-to-top' .each ->
+  e = $ this
+  e.attr 'title' 'Scroll to Top!'
+  e.on 'mousedown' -> # bouncy scroll to top
+    <- $ 'html,body' .animate { scroll-top:$ 'body' .offset!top }, 100
+    <- $ 'html,body' .animate { scroll-top:$ 'body' .offset!top+threshold }, 75
+    <- $ 'html,body' .animate { scroll-top:$ 'body' .offset!top }, 25
--- public/js/layout.js
+++ public/js/layout.js
@@ -1,11 +1,34 @@
 (function(){
-  var win;
-  win = $(window);
-  win.on('scroll', function(){
-    if (win.scrollTop() > 8) {
+  var threshold, w, hasScrolled;
+  threshold = 10;
+  w = $(window);
+  hasScrolled = function(){
+    if (w.scrollTop() > threshold) {
       return $('body').addClass('has-scrolled');
     } else {
       return $('body').removeClass('has-scrolled');
     }
+  };
+  w.on('scroll', function(){
+    return hasScrolled();
+  });
+  hasScrolled();
+  $('.scroll-to-top').each(function(){
+    var e;
+    e = $(this);
+    e.attr('title', 'Scroll to Top!');
+    return e.on('mousedown', function(){
+      return $('html,body').animate({
+        scrollTop: $('body').offset().top
+      }, 100, function(){
+        return $('html,body').animate({
+          scrollTop: $('body').offset().top + threshold
+        }, 75, function(){
+          return $('html,body').animate({
+            scrollTop: $('body').offset().top
+          }, 25, function(){});
+        });
+      });
+    });
   });
 }).call(this);
