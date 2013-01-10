5140d5a95ac5e42684cc9d2f582709a8c80493fe
Matthew Elder <matt@mattelder.org>
2013-01-09 17:13:22 -0800

    yay grunt works on vagrant and global npm install too

--- chef-recipes/myapp/recipes/default.rb
+++ chef-recipes/myapp/recipes/default.rb
@@ -14,21 +14,22 @@ include_recipe 'voltdb'
 include_recipe 'varnish'
 include_recipe 'stunnel'
 
-# lets install a few extra packages globally from npm (lets not get carried away kids)
-unless File.file? '/etc/NPM_GLOBAL_PACKAGES_COMPLETED'
-  bash 'install grunt, LiveScript' do
-    code <<-EOH
-      npm install -g git://github.com/gruntjs/grunt-cli.git
-      npm install -g LiveScript
-      touch /etc/NPM_GLOBAL_PACKAGES_COMPLETED
-    EOH
-  end
-end
-
 # XXX this should perhaps go somewhere else, but for now, i like having this always
 package 'tmux'
 package 'vim'
 package 'tree'
 package 'zsh'
 package 'git'
+
+# lets install a few extra packages globally from npm (lets not get carried away kids)
+unless File.file? '/etc/NPM_GLOBAL_PACKAGES_COMPLETED'
+  bash 'install grunt, LiveScript' do
+    code <<-EOH
+      set -e
+      npm install -g git://github.com/gruntjs/grunt-cli.git
+      npm install -g LiveScript
+      touch /etc/NPM_GLOBAL_PACKAGES_COMPLETED
+    EOH
+  end
+end
 
