4925e2320c2718e46a3fffd6209f5bb611cb79a5
Matthew Elder <matt@mattelder.org>
2013-01-12 21:35:08 -0800

    clojure recipe

---
+++ chef-recipes/clojure/recipes/default.rb
@@ -0,0 +1,17 @@
+unless File.directory? '/usr/local/clojure-1.4.0'
+  package 'openjdk-7-jdk'
+  package 'unzip'
+
+  bash 'installing clojure' do
+    cwd 'tmp'
+    code <<-EOH
+      set -e
+
+      wget http://repo1.maven.org/maven2/org/clojure/clojure/1.4.0/clojure-1.4.0.zip
+      cd /usr/local
+      unzip /tmp/clojure-1.4.0.zip
+      rm -rf clojure
+      ln -s clojure-1.4.0 clojure
+    EOH
+  end
+end
