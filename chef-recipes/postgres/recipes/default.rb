ba49907b61f66da70aad46746d0c8efd5d6bf8bf
Matthew Elder <matt@mattelder.org>
2013-01-14 20:24:09 -0800

    chef recipe for postgres 9.2, app/postgres.ls shim for queries

---
+++ chef-recipes/postgres/recipes/default.rb
@@ -0,0 +1,34 @@
+# followed instructions from www.postgresql.org/download/linux/ubuntu/
+unless File.file? '/usr/bin/psql'
+  cookbook_file '/etc/apt/sources.list.d/pgdg.list' do
+    source 'pgdg.list'
+    mode '0644'
+  end
+
+  cookbook_file '/etc/apt/preferences.d/pgdg.pref' do
+    source 'pgdg.pref'
+    mode '0644'
+  end
+
+  bash "installing pgdg repository (postgres official)" do
+    code <<-EOH
+      set -e
+      wget -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+
+      sudo apt-get update
+      sudo apt-get install pgdg-keyring postgresql-9.2 postgresql-server-dev-9.2 -y
+    EOH
+  end
+
+  cookbook_file '/etc/postgresql/9.2/main/pg_hba.conf' do
+    source 'pg_hba.conf'
+    mode '0644'
+  end
+
+  bash "restarting postgres (so custom config can be loaded)" do
+    code <<-EOH
+      set -e
+      sudo service postgresql restart
+    EOH
+  end
+end
