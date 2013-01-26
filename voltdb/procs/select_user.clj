2d1899eadefd4d0b83a68d26f31e02df83b6a870
Matthew Elder <matt@mattelder.org>
2013-01-26 15:59:11 -0800

    yay partitioned procedures in voltdb + clojure

--- voltdb/procs/select_user.clj
+++ voltdb/procs/select_user.clj
@@ -1,9 +1,9 @@
-(ns procs.select-users
+(ns select-user
   (:gen-class
    :extends org.voltdb.VoltProcedure
    :state state
    :init init
-   :methods [[run [] org.voltdb.VoltTable]
+   :methods [[run [long] org.voltdb.VoltTable]
              [statements [] java.util.Map]]))
  
 (defn stmt
@@ -12,16 +12,16 @@
 
 (defn init-stmts
   []
-  {"select" (stmt "SELECT * FROM users ORDER BY id")})
+  {"select" (stmt "SELECT * FROM users WHERE id=? LIMIT 1")})
 
 (defn -init
   []
   [[] (init-stmts)])
 
 (defn -run
-  [this]
-  (.voltQueueSQL this (get (.state this) "select") (into-array []))
-  (.voltExecuteSQL this))
+  [this & args]
+  (.voltQueueSQL this (get (.state this) "select") (into-array args))
+  (nth (.voltExecuteSQL this) 0))
 
 (defn -statements
   [this]
