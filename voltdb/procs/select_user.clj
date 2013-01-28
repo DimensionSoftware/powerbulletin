88f6176e34bbe90376e50ae59f5179e18f5c2006
Matthew Elder <matt@mattelder.org>
2013-01-28 01:42:28 -0800

    birth of 'defproc' macro for defining volt procedures

--- voltdb/procs/select_user.clj
+++ voltdb/procs/select_user.clj
@@ -1,28 +1,30 @@
-(ns select-user
-  (:gen-class
-   :extends org.voltdb.VoltProcedure
-   :state state
-   :init init
-   :methods [[run [long] org.voltdb.VoltTable]
-             [statements [] java.util.Map]]))
- 
-(defn stmt
-  [sql & args]
-  (new org.voltdb.SQLStmt sql args))
+(defmacro defproc [name param-types params statements & body]
+  `(do
+     (ns ~name
+       (:gen-class
+        :extends org.voltdb.VoltProcedure
+        :state ~'state
+        :init ~'init
+        :methods [[~'run ~param-types org.voltdb.VoltTable]
+                  [~'statements [] java.util.Map]]))
 
-(defn init-stmts
-  []
-  {"select" (stmt "SELECT * FROM users WHERE id=? LIMIT 1")})
+     (defn ~'stmt [sql# & args#]
+       (new org.voltdb.SQLStmt sql# args#))
 
-(defn -init
-  []
-  [[] (init-stmts)])
+     (defn ~'-init []
+       [[] ~statements])
 
-(defn -run
-  [this & args]
+     (defn ~'-statements [this#]
+       (.state this#))
+
+     (defn ~'-run
+       ~params
+       ~@body)))
+
+(defproc select-user [long] [this & args]
+  {"select" (stmt "SELECT * FROM users WHERE id=? LIMIT 1")}
+
   (.voltQueueSQL this (get (.state this) "select") (into-array args))
   (nth (.voltExecuteSQL this) 0))
 
-(defn -statements
-  [this]
-  (.state this))
+;(println (macroexpand '(defproc select-user)))
