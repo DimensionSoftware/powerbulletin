5a8469522e6db948ebaba2f7df9b66a539344da3
Matthew Elder <matt@mattelder.org>
2013-02-04 13:45:48 -0800

    move stubbing into stored procedures

--- voltdb/procs/build_homepage.clj
+++ voltdb/procs/build_homepage.clj
@@ -17,6 +17,48 @@
               ORDER BY created DESC, id DESC
               LIMIT 10")})
 
+
+(defn user-stub []
+  {"id" 1
+   "name" "intrepid_codergirl"
+   "created_at" (new java.util.Date)})
+
+(defn sub-post-stub [id]
+  {"id" id
+   "title" (str "Sub-Post Title " id)
+   "date" (new java.util.Date)
+   "body" "hello world from a stub post yo"
+   "user" (user-stub)})
+
+(defn post-stub [id]
+  {"id" id
+   "title" (str "Post Title " id)
+   "date" (new java.util.Date)
+   "body" "hello world"
+   "user" (user-stub)
+   "subposts" (map sub-post-stub (range 1 3))})
+
+(defn sub-forum-stub [id]
+  {"id" id
+   "theme" (if (= id 1) "light" "dark")
+   "title" (str "Sub-Forum " id)
+   "slug" (str "sub-forum-" id)
+   "description" (str "Description for Sub-Forum " id)
+   "posts" (map post-stub (range 1 3))})
+
+; depersonalized, non-time-aware for docs
+(defn forum-stub [id]
+  {"id" id
+   "theme" (if (= id 1) "light" "dark")
+   "title" (str "Forum " id)
+   "slug" (str "forum-" id)
+   "description" (str "Description for Forum " id)
+   "posts" (map post-stub (range 1 3))
+   "subforums" (map sub-forum-stub (range 1 3))})
+
+(defn homepage-stub []
+  {"forums" (map forum-stub (range 1 4))})
+
 (defn run [this now]
   (u/queue this "build-homepage-top-posts")
   ; TODO: additionally, queue up other queries needed to populate homepage data
@@ -24,8 +66,10 @@
   ; - toplevel forums
   ; - first level of subforums for each toplevel forum
   ; - what else guys??
+  ; XXX: REALLY use top-posts, not just pretend ; )
   (let [top-posts (u/vt2maplist (nth (u/execute this) 0))
-        out (u/obj2json top-posts)]
+        homepage (homepage-stub)
+        out (u/obj2json homepage)]
     (println out)
     ; upsert homepage doc
     (u/queue this "build-homepage-select")
