c183d747631b4b8bb10e67f810c402e9c2a488cb
Matthew Elder <matt@mattelder.org>
2013-02-06 15:36:54 -0800

    proof of concept get_user with arguments

--- plls-procedure-test.sql
+++ plls-procedure-test.sql
@@ -1,3 +1,5 @@
+-- FUNCTION: test
+DROP FUNCTION IF EXISTS test();
 CREATE OR REPLACE FUNCTION test() RETURNS SETOF int AS $$
   # test
   mylist =
@@ -10,4 +12,15 @@ CREATE OR REPLACE FUNCTION test() RETURNS SETOF int AS $$
   mylist2 = require(\mymod).foo!
 
   return mylist2
+$$ LANGUAGE plls IMMUTABLE STRICT;
+
+-- FUNCTION: get_user
+DROP FUNCTION IF EXISTS get_user(int);
+DROP TYPE IF EXISTS get_user_r;
+CREATE TYPE get_user_r AS
+  ( id bigint
+  , created timestamp
+  , updated timestamp);
+CREATE FUNCTION get_user(id int) RETURNS SETOF get_user_r AS $$
+  return plv8.execute('SELECT * FROM USERS WHERE id=$1', [id])
 $$ LANGUAGE plls IMMUTABLE STRICT;
