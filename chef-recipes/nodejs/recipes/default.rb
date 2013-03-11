df47f742cacb0a5e02ff73d5de1eff18820e86ea
John BEPPU <john.beppu@gmail.com>
2013-03-11 15:18:27 -0700

    node 0.8.19 to 0.8.22

--- chef-recipes/nodejs/recipes/default.rb
+++ chef-recipes/nodejs/recipes/default.rb
@@ -1,11 +1,11 @@
 
-ver = '0.8.19'
+ver = '0.8.22'
 
 unless File.file? '/usr/local/bin/node'
   package 'build-essential'
 
-  remote_file '/tmp/node-v0.8.19.tar.gz' do
-    source 'http://nodejs.org/dist/v0.8.19/node-v0.8.19.tar.gz'
+  remote_file '/tmp/node-v0.8.22.tar.gz' do
+    source 'http://nodejs.org/dist/v0.8.22/node-v0.8.22.tar.gz'
     checksum '703207d7b394bd3d4035dc3c94b417ee441fd3ea66aa90cd3d7c9bb28e5f9df4'
   end
 
@@ -13,8 +13,8 @@ unless File.file? '/usr/local/bin/node'
     cwd '/tmp'
     code <<-EOH
       set -e
-      tar -xzf node-v0.8.19.tar.gz
-      cd node-v0.8.19
+      tar -xzf node-v0.8.22.tar.gz
+      cd node-v0.8.22
       ./configure
       make
       make install
