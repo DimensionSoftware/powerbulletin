ad1c71406bf873440c1efd0799391f13d144892b
Matthew Elder <matt@mattelder.org>
2013-03-28 15:55:59 -0700

    install soda

--- chef-recipes/myapp/recipes/default.rb
+++ chef-recipes/myapp/recipes/default.rb
@@ -40,7 +40,7 @@ unless File.file? '/etc/NPM_GLOBAL_PACKAGES_COMPLETED'
       npm install -g git://github.com/gruntjs/grunt-cli.git
       npm install -g LiveScript
       npm install -g contextify
-      npm install -g mocha-phantomjs
+      npm install -g mocha
       npm install -g chai
       touch /etc/NPM_GLOBAL_PACKAGES_COMPLETED
     EOH
