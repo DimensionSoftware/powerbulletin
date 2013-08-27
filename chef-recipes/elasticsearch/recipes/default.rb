97d87ee57fbee68b135cd7c5256c6a0a5cefecf8
Matthew Elder <matt@mattelder.org>
2013-08-26 19:03:35 -0700

    upgrade elasticsearch to 0.90.3

--- chef-recipes/elasticsearch/recipes/default.rb
+++ chef-recipes/elasticsearch/recipes/default.rb
@@ -1,4 +1,4 @@
-unless File.file? '/opt/elasticsearch-0.20.6/bin/elasticsearch'
+unless File.file? '/opt/elasticsearch-0.90.3/bin/elasticsearch'
   package 'openjdk-7-jdk'
 
   bash "installing elasticsearch" do
@@ -6,10 +6,12 @@ unless File.file? '/opt/elasticsearch-0.20.6/bin/elasticsearch'
     code <<-EOH
       set -e
 
-      wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.6.tar.gz
+      wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.tar.gz
       cd /opt
-      tar -xvzf /tmp/elasticsearch-0.20.6.tar.gz
-      ln -s elasticsearch-0.20.6 elasticsearch &> /dev/null
+      tar -xvzf /tmp/elasticsearch-0.90.3.tar.gz
+      rm -f elasticsearch
+      ln -s elasticsearch-0.90.3 elasticsearch &> /dev/null
+      mv elasticsearch/config elasticsearch/config-stock
     EOH
   end
 end
